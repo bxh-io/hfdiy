2864299fe53c7f08348cf46f20928f8c6ddf6002
Ben Hall <ben.hall@blikbook.com>
2017-01-04 10:22:11 +0000

    Updates

--- static/js/40c3726c.js
+++ static/js/40c3726c.js
@@ -65,7 +65,7 @@
 /******/ 	}
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "fed938f31a3e23022f00"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "40c3726cdc26043561d4"; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentParents = []; // eslint-disable-line no-unused-vars
 /******/ 	
@@ -570,7 +570,7 @@
 /******/ 	__webpack_require__.c = installedModules;
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/";
+/******/ 	__webpack_require__.p = "/hf-diy/";
 /******/
 /******/ 	// __webpack_hash__
 /******/ 	__webpack_require__.h = function() { return hotCurrentHash; };
@@ -34821,4 +34821,4 @@
 
 /***/ }
 /******/ ]);
-//# sourceMappingURL=fed938f3.js.map
+//# sourceMappingURL=40c3726c.js.map
